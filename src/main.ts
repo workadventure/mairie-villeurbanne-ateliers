3bbb260d1b62231f23569ce31e5ed7b8803f5844
Valdo <vteixeiraromao@gmail.com>
2023-06-08 15:49:54 +0100

    Fixed config

--- src/main.ts
+++ src/main.ts
@@ -7,40 +7,40 @@ console.log('Script started successfully');
 let currentPopup: any = undefined;
 let currentZone: string;
 
+const config = [
+    {
+        zone: 'needHelp',
+        message: 'Do you need some guidance? We are happy to help you out.',
+        cta: [
+            {
+                label: 'Meet us',
+                className: 'primary',
+                callback: () => WA.openTab('https://play.staging.workadventu.re/@/tcm/workadventure/wa-village'),
+            }
+        ]
+    },
+    {
+        zone: 'followUs',
+        message: 'Hey! Have you already started following us?',
+        cta: [
+            {
+                label: 'LinkedIn',
+                className: 'primary',
+                callback: () => WA.openTab('https://www.linkedin.com/company/workadventu-re'),
+            },
+            {
+                label: 'Twitter',
+                className: 'primary',
+                callback: () => WA.openTab('https://twitter.com/workadventure_'),
+            }
+        ]
+    },
+]
+
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
-
-    const config = [
-        {
-            zone: 'needHelp',
-            message: 'Do you need some guidance? We are happy to help you out.',
-            cta: [
-                {
-                    label: 'Meet us',
-                    className: 'primary',
-                    callback: () => WA.openTab('https://play.staging.workadventu.re/@/tcm/workadventure/wa-village'),
-                }
-            ]
-        },
-        {
-            zone: 'followUs',
-            message: 'Hey! Have you already started following us?',
-            cta: [
-                {
-                    label: 'LinkedIn',
-                    className: 'primary',
-                    callback: () => WA.openTab('https://www.linkedin.com/company/workadventu-re'),
-                },
-                {
-                    label: 'Twitter',
-                    className: 'primary',
-                    callback: () => WA.openTab('https://twitter.com/workadventure_'),
-                }
-            ]
-        },
-    ]
 
     // Show configuration tile for editors only
     if (WA.player.tags.includes('editor')) {

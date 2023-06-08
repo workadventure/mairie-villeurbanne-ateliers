036dfb67a6a3c9967938ba9e2fcc024cdfe2684e
Valdo <vteixeiraromao@gmail.com>
2023-06-08 15:38:28 +0100

    Imported and updated workshop map

--- src/main.ts
+++ src/main.ts
@@ -5,19 +5,61 @@ import { bootstrapExtra } from "@workadventure/scripting-api-extra";
 console.log('Script started successfully');
 
 let currentPopup: any = undefined;
+let currentZone: string;
 
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
-    WA.room.area.onEnter('clock').subscribe(() => {
-        const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup", "It's " + time, []);
-    })
+    const config = [
+        {
+            zone: 'needHelp',
+            message: 'Do you need some guidance? We are happy to help you out.',
+            cta: [
+                {
+                    label: 'Meet us',
+                    className: 'primary',
+                    callback: () => WA.openTab('https://play.staging.workadventu.re/@/tcm/workadventure/wa-village'),
+                }
+            ]
+        },
+        {
+            zone: 'followUs',
+            message: 'Hey! Have you already started following us?',
+            cta: [
+                {
+                    label: 'LinkedIn',
+                    className: 'primary',
+                    callback: () => WA.openTab('https://www.linkedin.com/company/workadventu-re'),
+                },
+                {
+                    label: 'Twitter',
+                    className: 'primary',
+                    callback: () => WA.openTab('https://twitter.com/workadventure_'),
+                }
+            ]
+        },
+    ]
 
-    WA.room.area.onLeave('clock').subscribe(closePopup)
+    // Show configuration tile for editors only
+    if (WA.player.tags.includes('editor')) {
+        WA.room.showLayer('exitNorthConfig')
+        WA.room.showLayer('exitSouthConfig')
+        WA.room.showLayer('exitWestConfig')
+        WA.room.showLayer('exitEastConfig')
+    }
+
+    WA.onEnterZone('needHelp', () => {
+        currentZone = 'needHelp'
+        openPopup(currentZone, currentZone + 'Popup')
+    });
+    WA.onEnterZone('followUs', () => {
+        currentZone = 'followUs'
+        openPopup(currentZone, currentZone + 'Popup')
+    });
+    WA.onLeaveZone('needHelp', closePopup);
+    WA.onLeaveZone('followUs', closePopup);
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
@@ -25,6 +67,16 @@ WA.onInit().then(() => {
     }).catch(e => console.error(e));
 
 }).catch(e => console.error(e));
+
+function openPopup(zoneName: string, popupName: string) {
+    const zone = config.find((item) => {
+        return item.zone == zoneName
+    });
+    if (typeof zone !== 'undefined') {
+        // @ts-ignore otherwise we can't use zone.cta object
+        currentPopup = WA.openPopup(popupName, zone.message, zone.cta)
+    }
+}
 
 function closePopup(){
     if (currentPopup !== undefined) {
